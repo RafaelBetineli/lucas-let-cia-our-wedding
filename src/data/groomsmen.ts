cdb34e200d8720734c1971bde6b6de1acacbc5db
Rafael Betineli da Silva <rafaelbetineli@outlook.com>
2026-07-01 11:27:13 -0300

    Adicionado seção e fotos reais dos padrinhos.

--- src/data/groomsmen.ts
+++ src/data/groomsmen.ts
@@ -2,6 +2,15 @@ import foto01 from "@/assets/groomsmen/padrinhos-01.jpg";
 import foto02 from "@/assets/groomsmen/padrinhos-02.jpg";
 import foto03 from "@/assets/groomsmen/padrinhos-03.jpg";
 import foto04 from "@/assets/groomsmen/padrinhos-04.jpg";
+import foto05 from "@/assets/groomsmen/padrinhos-05.jpg";
+import foto06 from "@/assets/groomsmen/padrinhos-06.jpg";
+import foto07 from "@/assets/groomsmen/padrinhos-07.jpg";
+import foto08 from "@/assets/groomsmen/padrinhos-08.jpg";
+import foto09 from "@/assets/groomsmen/padrinhos-09.jpg";
+import foto10 from "@/assets/groomsmen/padrinhos-10.jpg";
+import foto11 from "@/assets/groomsmen/padrinhos-11.jpg";
+import foto12 from "@/assets/groomsmen/padrinhos-12.jpg";
+import foto13 from "@/assets/groomsmen/padrinhos-13.jpg";
 
 export const groomsmen = [
   {
@@ -9,15 +18,51 @@ export const groomsmen = [
     image: foto01,
   },
   {
-    name: "Rafael e Barbara",
+    name: "Rafael e Bárbara",
     image: foto02,
   },
   {
-    name: "Erik e Julia",
+    name: "Erik e Júlia",
     image: foto03,
   },
   {
     name: "Daniel e Débora",
     image: foto04,
   },
+  {
+    name: "Alessandro e Sabrina",
+    image: foto05,
+  },
+   {
+    name: "Priscila",
+    image: foto06,
+  },
+  {
+    name: "Bruna",
+    image: foto07,
+  },
+  {
+    name: "Érika",
+    image: foto08,
+  },
+  {
+    name: "Thiago",
+    image: foto09,
+  },
+  {
+    name: "Felipe",
+    image: foto10,
+  },
+   {
+    name: "Guilherme",
+    image: foto11,
+  },
+  {
+    name: "Joana",
+    image: foto12,
+  },
+  {
+    name: "Matheus e Larissa",
+    image: foto13,
+  }
 ];
